e22c9674bd48cc242abee97d561d00305a3ed4d0
jaredjhz <jaredjhz@gmail.com>
2021-01-20 15:10:52 -0600

    added v0.1

---
+++ posts/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+
+const axios = require('axios')
+
+const {randomBytes} = require('crypto')
+
+const cors = require('cors')
+
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const posts = {}
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/posts', async (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const {title} = req.body;
+
+    posts[id] = {
+        id, title
+    }
+
+    await axios.post('http://localhost:4005/events', {
+        type:"PostCreated",
+        data: {
+            id,
+            title
+        }
+    })
+
+    res.status(201).send(posts[id]) 
+})
+
+app.post('/events', (req, res) => {
+    console.log('received event', req.body.type)
+    res.status(200).send({})
+})
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000')
+})
